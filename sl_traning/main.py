bada3a8d9d5312969c205263491041c8ecc5f35d
vutrungnghia <nghia.vt173284@sis.hust.edu.vn>
2020-04-23 20:57:33 +0700

    change-loss-to-valid-moves

--- sl_traning/main.py
+++ sl_traning/main.py
@@ -14,17 +14,19 @@ from model import ChessModel
 from dataset import FICSDataset
 from utils import read_yaml
 
-logging.basicConfig(filename='logs.txt',
-                    filemode='a',
-                    format='%(asctime)s, %(levelname)s: %(message)s',
-                    datefmt='%y-%m-%d %H:%M:%S',
-                    level=logging.DEBUG)
-console = logging.StreamHandler()
-console.setLevel(logging.INFO)
-logging.getLogger().addHandler(console)
+# logging.basicConfig(filename='logs.txt',
+#                     filemode='a',
+#                     format='%(asctime)s, %(levelname)s: %(message)s',
+#                     datefmt='%y-%m-%d %H:%M:%S',
+#                     level=logging.INFO)
+# console = logging.StreamHandler()
+# console.setLevel(logging.INFO)
+# logging.getLogger().addHandler(console)
+logging.basicConfig(level=logging.INFO)
 
 configs = read_yaml('configs.yml')
-
+logging.info('\n\n========== ALPHAZERO ========\n\n')
+logging.info(configs)
 
 def read_data(sets: dict):
     s = []
@@ -55,17 +57,20 @@ def loss_func(pred: torch.tensor, valid_moves: torch.tensor, y: torch.tensor) ->
     valid_moves: N x C
     y: N
     """
-#     pred = pred * (valid_moves == 1)
+    pred = pred + (valid_moves == 0) * (-20)
     loss_func = nn.CrossEntropyLoss()
     return loss_func(pred, y)
 
 train_dataset = FICSDataset(read_data(configs['train']))
-train_dataloader = DataLoader(train_dataset, batch_size=128, shuffle=True)
+train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 valid_dataset = FICSDataset(read_data(configs['valid']))
-valid_dataloader = DataLoader(valid_dataset, batch_size=128, shuffle=True)
+valid_dataloader = DataLoader(valid_dataset, batch_size=32, shuffle=True)
 
 model = ChessModel()
 model = model.cuda()
+if configs['resume']:
+    checkpoint = torch.load(configs['resume'])
+    model.load_state_dict(checkpoint['state_dict'])
 
 optimizer = torch.optim.Adam(model.parameters(),
                              lr=0.001,
@@ -74,10 +79,11 @@ optimizer = torch.optim.Adam(model.parameters(),
                              weight_decay=0,
                              amsgrad=False)
 
-for epoch in range(10):
+evaluate(valid_dataloader, model)
+for epoch in range(50):
     logging.info('===================')
     logging.info(f'EPOCH: {epoch}')
-    for idx, (state, valid_actions, y) in enumerate(train_dataloader):
+    for (state, valid_actions, y) in tqdm(train_dataloader):
         model.train()
         state = state.cuda()
         valid_actions = valid_actions.cuda()
@@ -87,14 +93,11 @@ for epoch in range(10):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        if idx % 10 == 0:
-            logging.info(f'Iter {idx}/{len(train_dataloader)} - loss: {loss.item()}')
 
-        if idx % 100 == 0:
-            evaluate(valid_dataloader, model)
-            x = datetime.datetime.now()
-            time = x.strftime("%y-%m-%d_%H:%M:%S")
-            model_checkpoint = os.path.join('models', f'checkpoint_{time}_{epoch}_{idx}.pth')
-            with torch.no_grad():
-                torch.save({'epoch': epoch + 1, 'state_dict': model.state_dict()}, model_checkpoint)
+    x = datetime.datetime.now()
+    time = x.strftime("%y-%m-%d_%H:%M:%S")
+    model_checkpoint = os.path.join('/content/gdrive/My Drive/MOUNT/models', f'checkpoint_{time}_{epoch}.pth')
+    logging.info(model_checkpoint)
+    with torch.no_grad():
+        torch.save({'epoch': epoch + 1, 'state_dict': model.state_dict()}, model_checkpoint)
     evaluate(valid_dataloader, model)
